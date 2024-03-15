e2b47d6d0370ef0de02fdd45eb0135e5448a0031
Mizu <psdk55052004@gmail.com>
2024-03-15 16:00:03 +0800

    新增 Vue CKEditor

---
+++ src/components/VueCKEditor.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+// data
+const editor = ref(ClassicEditor)
+const editorData = ref('<p>Content of the editor.</p>')
+const editorConfig = ref({
+  // The configuration of the editor.
+})
+
+watchEffect(() => {
+  console.log('內容改變', editorData.value)
+})
+
+</script>
+
+<template>
+<Ckeditor :editor="editor" v-model="editorData" :config="editorConfig" />
+
+{{ editorData }}
+</template>
+
+<style lang='scss' scope></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,14 @@
     <h3 class="text-20 mb-3">Js 版本</h3>
     <CKEditor />
   </div>
+  <div class="text-center my-10">
+    <h3 class="text-20 mb-3">Vue 版本</h3>
+    <VueCKEditor />
+  </div>
 </template>
 
 <script setup>
+import VueCKEditor from '@/components/VueCKEditor.vue'
 import CKEditor from '@/components/CKEditor.vue'
 </script>
 
