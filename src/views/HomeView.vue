b2deffbc32907962c5d871df405dbf5a3d2f2231
Mizu <psdk55052004@gmail.com>
2024-03-20 15:49:09 +0800

    新增 CKEditorCustom 官方自訂功能款 練習

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,24 @@
 
     <!-- <CKEditorClassic /> -->
 
+    <!-- <CKEditorDocument /> -->
+
     <CKEditorDocument />
   </div>
 </template>
 
 <script setup>
 // import CKEditor from '@/components/CKEditor.vue'
+
+// 官方經典款
 // import CKEditorClassic from '@/components/進階-匯入更多文字功能模組/CKEditorClassic.vue'
-import CKEditorDocument from '@/components/進階-匯入更多文字功能模組/CKEditorDocument.vue'
+
+// 官方文檔款
+// import CKEditorDocument from '@/components/進階-匯入更多文字功能模組/CKEditorDocument.vue'
+
+// 官方自訂版款
+import CKEditorDocument from '@/components/進階-匯入更多文字功能模組/CKEditorCustom.vue'
+
 </script>
 
 <style lang='scss' scope></style>
