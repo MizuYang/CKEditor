50f36d2e91c34ee7a968e9d78bcf9a25deda2037
Mizu <psdk55052004@gmail.com>
2024-03-20 11:45:27 +0800

    新增 CKEditorDocument 官方文檔款 練習

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <!-- <div class="text-center my-10">
-    <h3 class="text-20 mb-3">Js 版本</h3>
-    <CKEditor />
-  </div> -->
-
   <div class="text-center my-10">
-    <h3 class="text-20 mb-3">官方經典版本</h3>
-    <CKEditorClassic />
+    <!-- 不同的編輯器無法共存 -->
+    <!-- <CKEditor /> -->
+
+    <!-- <CKEditorClassic /> -->
+
+    <CKEditorDocument />
   </div>
 </template>
 
 <script setup>
 // import CKEditor from '@/components/CKEditor.vue'
-import CKEditorClassic from '@/components/進階-匯入更多文字功能模組/CKEditorClassic.vue'
+// import CKEditorClassic from '@/components/進階-匯入更多文字功能模組/CKEditorClassic.vue'
+import CKEditorDocument from '@/components/進階-匯入更多文字功能模組/CKEditorDocument.vue'
 </script>
 
 <style lang='scss' scope></style>
