bb5d6a71633e839fd35a474eb5caeb9e5d8c4062
Mizu <psdk55052004@gmail.com>
2024-03-20 16:08:14 +0800

    新增 CKEditorCustom 多種文字功能款 練習

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,17 @@
 
     <!-- <CKEditorDocument /> -->
 
-    <CKEditorDocument />
+    <!-- <CKEditorDocument /> -->
+
+    <CKEditorRich />
   </div>
 </template>
 
 <script setup>
 // import CKEditor from '@/components/CKEditor.vue'
+
+// 多種文字功能款
+import CKEditorRich from '@/components/進階-匯入更多文字功能模組/CKEditorRich.vue'
 
 // 官方經典款
 // import CKEditorClassic from '@/components/進階-匯入更多文字功能模組/CKEditorClassic.vue'
@@ -21,7 +26,7 @@
 // import CKEditorDocument from '@/components/進階-匯入更多文字功能模組/CKEditorDocument.vue'
 
 // 官方自訂版款
-import CKEditorDocument from '@/components/進階-匯入更多文字功能模組/CKEditorCustom.vue'
+// import CKEditorDocument from '@/components/進階-匯入更多文字功能模組/CKEditorCustom.vue'
 
 </script>
 
